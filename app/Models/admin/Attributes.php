1964a7ca2bfe0ccbab20d36e9295a9dd49f984e3
Lê Tuấn Anh <anhltph51239@gmail.com>
2025-11-14 21:19:10 +0700

    sua model

--- app/Models/admin/Attributes.php
+++ app/Models/admin/Attributes.php
@@ -9,7 +9,7 @@ class Attributes extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug'];
+    protected $fillable = ['name', 'slug','created_at','updated_at'];
 
     public function values()
     {
